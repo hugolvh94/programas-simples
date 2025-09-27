9840adfaecc5d88bbaa26f4576b29fcd517ba45a
Victor Hugo <victor.vh.andrade@gmail.com>
2025-09-27 10:44:03 -0300

    Add files via upload

---
+++ problema2.js
@@ -0,0 +1,4 @@
+let r = Number(prompt('Informe a resistencia do circuito:'));
+let i = Number(prompt('Informe a corrente elétrica qua passa pelo circuito:'));
+let u = r * i 
+console.log('A d.d.p total do circuito será: ', u,'V');
